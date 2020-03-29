c32dee230441fe8b673916e323ac822e2e639aaf
Duc Nguyen <nguoihanhtinh123@gmail.com>
2020-03-29 15:49:32 +0700

    add static root

--- blog/settings.py
+++ blog/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Setting email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
