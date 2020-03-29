860431cba70f1b12a2d0a778dd9f7e118e5c9cc6
Duc Nguyen <nguoihanhtinh123@gmail.com>
2020-03-29 15:30:36 +0700

    [Modify] base.html

--- blog/settings.py
+++ blog/settings.py
@@ -126,6 +126,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ducnguyenpy1221@gmail.com'
-EMAIL_HOST_PASSWORD = 'huynhducpy1'
+
+from .user_account import(
+    EMAIL_HOST_USER as email,
+    EMAIL_HOST_PASSWORD as password,
+)
+EMAIL_HOST_USER = email
+EMAIL_HOST_PASSWORD = password
 EMAIL_USE_SSL = False
