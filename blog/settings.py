7b2b82a246e96cb77ac4412a1c5f7360856ca88a
Duc Nguyen <nguoihanhtinh123@gmail.com>
2020-03-30 16:47:22 +0700

    Edit

--- blog/settings.py
+++ blog/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True = os.environ.get('DEBUG_VALUE' == 'True')
+DEBUG = True
 
-ALLOWED_HOSTS = ['alykes.herokuapp.com']
-
+# ALLOWED_HOSTS = ['alykes.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
